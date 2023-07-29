104260fa828ef996bad286fd4193c19e58f694c4
Portles <nizamet005@gmail.com>
2023-07-30 01:20:06 +0300

    Colors Documented

--- Sources/MetallicView/MetallicView.swift
+++ Sources/MetallicView/MetallicView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 public extension View {
-    // 0.0 - 10.0
-    func metallicbrightness(brightness: Float = 0.0, isEnabled: Bool = true) -> some View {
+    
+    /**
+     This method brightens colors and returns to view.
+
+     This methods adds two value and return the View.
+     Brightness value recommended between 0.0 - 10.0
+
+     - parameter brightness: Brightness control.
+     - parameter isEnabled: Enable control.
+     - returns: Brightened colors.
+
+     # Notes: #
+     1. First Parameter must be **CGFloat** type
+     2. Second Parameter must be **Bool** type
+     3. Must return on a view.
+
+     # Example #
+    ```
+    View()
+     .metallicbrightness(brightness: $brightness, isEnabled: $isEnabled)
+     ```
+    */
+    func metallicbrightness(brightness: CGFloat = 0.0, isEnabled: Bool = true) -> some View {
         self
             .colorEffect(Shader(function: .init(library: .default, name: MetallicFunctions.brightness.rawValue), arguments: [.float(brightness)]), isEnabled: isEnabled)
     }
@@ -17,19 +38,19 @@ public extension View {
     /**
      This method invert color and returns to view.
 
-     Here is the discussion. This methods adds two double and return the optional Double.
+     This method use enable control and return the View.
 
      - parameter isEnabled: Enable control.
      - returns: Inverted color.
 
      # Notes: #
-     1. First Parameter must be **bool** type
+     1. First Parameter must be **Bool** type
      2. Must return on a view.
 
      # Example #
     ```
     View()
-     .metallicinvert(isEnabled: $isEnabled)
+     .metallicinvert(isEnabled: $enabled)
      ```
     */
     func metallicInvert(isEnabled: Bool = true) -> some View {
@@ -37,37 +58,175 @@ public extension View {
             .colorEffect(Shader(function: .init(library: .default, name: MetallicFunctions.invert.rawValue), arguments: []), isEnabled: isEnabled)
     }
     
+    /**
+     This method grayscale colors and returns to view.
+
+     This method use enable control and return the View.
+        
+     - parameter isEnabled: Enable control.
+     - returns: Grayscaled colors.
+
+     # Notes: #
+     1. First Parameter must be **Bool** type
+     2. Must return on a view.
+
+     # Example #
+    ```
+    View()
+     .metallicGrayscale(isEnabled: $enabled)
+     ```
+    */
     func metallicGrayscale(isEnabled: Bool = true) -> some View {
         self
             .colorEffect(Shader(function: .init(library: .default, name: MetallicFunctions.grayscale.rawValue), arguments: []), isEnabled: isEnabled)
     }
-    // 0.0 - 1.0
-    func metallicContrast(contrast: Float = 0.5,isEnabled: Bool = true) -> some View {
+    
+    /**
+     This method get contrast value and returns to view.
+
+     This method get contrast value and use enable control to return the View.
+     Contrast value recommended between 0.0 - 1.0
+        
+     - parameter contrast: Contrast control.
+     - parameter isEnabled: Enable control.
+     - returns: Contrast setted colors.
+
+     # Notes: #
+     1. First Parameter must be **CGFloat** type
+     2. Second Parameter must be **Bool** type
+     3. Must return on a view.
+
+     # Example #
+    ```
+    View()
+     .metallicContrast(contrast: $contrast, isEnabled: $enabled)
+     ```
+    */
+    func metallicContrast(contrast: CGFloat = 0.5,isEnabled: Bool = true) -> some View {
         self
             .colorEffect(Shader(function: .init(library: .default, name: MetallicFunctions.contrast.rawValue), arguments: [.float(contrast)]), isEnabled: isEnabled)
     }
     
+    /**
+     This method changes view colors to sepia and returns to view.
+
+     This method use enable control and return the View.
+        
+     - parameter isEnabled: Enable control.
+     - returns: Sepiadic colors.
+
+     # Notes: #
+     1. First Parameter must be **Bool** type
+     2. Must return on a view.
+        
+     # Example #
+    ```
+    View()
+     .metallicSepia(isEnabled: $enabled)
+     ```
+    */
     func metallicSepia(isEnabled: Bool = true) -> some View {
         self
             .colorEffect(Shader(function: .init(library: .default, name: MetallicFunctions.sepia.rawValue), arguments: []), isEnabled: isEnabled)
     }
-    // 0.0 - 50.0
-    func metallicDesaturate(desaturationLevel: Float = 5.0,isEnabled: Bool = true) -> some View {
+    
+    /**
+     This method change desaturation of view colors and returns to view.
+
+     This method use enable control and get desaturation level to return the View.
+     Recommended to set desaturation level on 0.0 - 50.0
+     
+     - parameter desaturationLevel: Desaturation control.
+     - parameter isEnabled: Enable control.
+     - returns: Desaturated colors.
+
+     # Notes: #
+     1. First Parameter must be **CGFloat** type
+     2. Second Parameter must be **Bool** type
+     3. Must return on a view.
+        
+     # Example #
+    ```
+    View()
+     .metallicDesaturate(desaturationLevel: $desaturation, isEnabled: $enabled)
+     ```
+    */
+    func metallicDesaturate(desaturationLevel: CGFloat = 5.0, isEnabled: Bool = true) -> some View {
         self
             .colorEffect(Shader(function: .init(library: .default, name: MetallicFunctions.desaturate.rawValue), arguments: [.float(desaturationLevel)]), isEnabled: isEnabled)
     }
-    // 0.0 - 1.0
-    func metallicThreshold(threshold: Float = 0.1,isEnabled: Bool = true) -> some View {
+    
+    /**
+     This method get threshold level and returns to view.
+
+     This method use enable control and get threshold level to return the View.
+     Recommended to set threshold level on 0.0 - 1.0
+     
+     - parameter threshold: Threshold control.
+     - parameter isEnabled: Enable control.
+     - returns: Thresholded colors.
+
+     # Notes: #
+     1. First Parameter must be **CGFloat** type
+     2. Second Parameter must be **Bool** type
+     3. Must return on a view.
+        
+     # Example #
+    ```
+    View()
+     .metallicThreshold(threshold: $threshold, isEnabled: $enabled)
+     ```
+    */
+    func metallicThreshold(threshold: CGFloat = 0.1,isEnabled: Bool = true) -> some View {
         self
             .colorEffect(Shader(function: .init(library: .default, name: MetallicFunctions.threshold.rawValue), arguments: []), isEnabled: isEnabled)
     }
     
+    /**
+     This method swaps red and blue color and returns to view.
+
+     This method use enable control to return the View.
+     
+     - parameter isEnabled: Enable control.
+     - returns: Swaped colors.
+
+     # Notes: #
+     1. First Parameter must be **Bool** type
+     2. Must return on a view.
+        
+     # Example #
+    ```
+    View()
+     .metallicColorChannelSwap(isEnabled: $enabled)
+     ```
+    */
     func metallicColorChannelSwap(isEnabled: Bool = true) -> some View {
         self
             .colorEffect(Shader(function: .init(library: .default, name: MetallicFunctions.colorChannelSwap.rawValue), arguments: []), isEnabled: isEnabled)
     }
-    // 0.0 - 1.0
-    func metallicGhost(ghost: Float = 0.2, isEnabled: Bool = true) -> some View {
+    
+    /**
+     This method get ghost level and returns to view.
+
+     This method use enable control and get ghost level to return the View.
+     Recommended to set ghost level on 0.0 - 1.0
+     
+     - parameter ghost: Ghost control.
+     - parameter isEnabled: Enable control.
+     - returns: Ghosted colors.
+
+     # Notes: #
+     1. First Parameter must be **CGFloat** type
+     2. Second Parameter must be **Bool** type
+     3. Must return on a view.
+        
+     # Example #
+    ```
+    View()
+     .metallicGhost(ghost: $ghost, isEnabled: $enabled)
+     ```
+    */
+    func metallicGhost(ghost: CGFloat = 0.2, isEnabled: Bool = true) -> some View {
         self
             .colorEffect(Shader(function: .init(library: .default, name: MetallicFunctions.ghost.rawValue), arguments: []), isEnabled: isEnabled)
     }
