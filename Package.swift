74dda237f988b5e9754ee1bdea94994a6b8e892c
Portles <nizamet005@gmail.com>
2023-07-31 00:21:15 +0300

    Could you work????

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
+        .executable(
             name: "MetallicView",
             targets: ["MetallicView"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
+        .executableTarget(
             name: "MetallicView"
         )
     ]
