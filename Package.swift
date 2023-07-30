a952df9aa2a9538b0d252d0eaad49ccb55b5df45
Portles <nizamet005@gmail.com>
2023-07-31 00:25:27 +0300

    Trying for second time

--- Package.swift
+++ Package.swift
@@ -10,15 +10,16 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .executable(
+        .library(
             name: "MetallicView",
             targets: ["MetallicView"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "MetallicView"
+        .target(
+            name: "MetallicView",
+            resources: [.process("color.metal")]
         )
     ]
 )
